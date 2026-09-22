d034f7fcaeae1381a281e8968854cdbc8bb094cb
Santosh Katageri <santoshpkatageri@gmail.com>
2026-09-22 22:11:27 +0530

    added Github Action CI pipeline

--- src/app.py
+++ src/app.py
@@ -14,6 +14,7 @@ def details():
     return jsonify({
         "name": "ship-boring",
         "version": "0.0.1",
+        "message": "This is a sample Python application",
         "time": datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S"),
         "Hostname": socket.gethostname()
     })
